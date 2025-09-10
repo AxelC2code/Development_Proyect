4830d7c5fa2e2dd95800431e70783d08ca382a3f
AxelC2 <caxel0624@gmail.com>
2025-09-09 23:26:51 -0500

    feat: add base form for classroom exercises

--- Sem_03_Interfaces_gráficas/Sem_03_Interfaces_gráficas/Program.cs
+++ Sem_03_Interfaces_gráficas/Sem_03_Interfaces_gráficas/Program.cs
@@ -11,7 +11,7 @@ namespace Sem_03_Interfaces_gráficas
             // To customize application configuration such as set high DPI settings or default font,
             // see https://aka.ms/applicationconfiguration.
             ApplicationConfiguration.Initialize();
-            Application.Run(new Form1());
+            Application.Run(new frmEjemplo3());
         }
     }
 }

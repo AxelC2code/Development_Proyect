2f9bd0938e98ccf9ccef248fac804fe89d9846dd
AxelC2 <caxel0624@gmail.com>
2025-09-20 23:55:39 -0500

    feat: add test week 2

---
+++ Sem_02_Operadores_Sobrecargados/ClsEquiposSobrecargado/Program.cs
@@ -0,0 +1,38 @@
+﻿namespace ClsEquiposSobrecargado
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            var Manucci = new Equipo("Manucci", "15% Hinchada", 100);
+            var Cienciano = new Equipo("Cienciano", "30% Hinchada", 300);
+            //Console.WriteLine(Manucci.ToString() );
+            //Console.WriteLine(Cienciano.ToString());
+
+            if (Manucci > Cienciano)
+                Console.WriteLine(Manucci);
+            else
+                Console.WriteLine(Cienciano);
+        }
+
+        public class Equipo
+        {
+            public string nombre { get; set; }
+            public string porcentaje { get; set; }
+            public int nrohinchas { get; set; }
+            public Equipo(string nombre, string porcentaje, int nrohinchas)
+            {
+                this.nombre = nombre;
+                this.porcentaje = porcentaje;
+                this.nrohinchas = nrohinchas;
+            }
+            public static bool operator <(Equipo a, Equipo b) => a.nrohinchas < b.nrohinchas;
+            public static bool operator >(Equipo a, Equipo b) => a.nrohinchas > b.nrohinchas;
+
+            public override string ToString()
+            {
+                return String.Format($"{nombre} {porcentaje}");
+            }
+        }
+    }
+}

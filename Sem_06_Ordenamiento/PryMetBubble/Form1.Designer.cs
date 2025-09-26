72087d40480d0856df38c78bdde5cf504f05946b
AxelC2 <caxel0624@gmail.com>
2025-09-26 14:46:58 -0500

    Add initial PryMetBubble WinForms project
    Created the PryMetBubble C# WinForms application with basic structure, including Form1, a data class (clsDatos), and all necessary project and resource files. The solution and project files are set up for .NET Framework 4.7.2.

---
+++ Sem_06_Ordenamiento/PryMetBubble/Form1.Designer.cs
@@ -0,0 +1,277 @@
+﻿namespace PryMetBubble
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Variable del diseñador necesaria.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Limpiar los recursos que se estén usando.
+        /// </summary>
+        /// <param name="disposing">true si los recursos administrados se deben desechar; false en caso contrario.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Código generado por el Diseñador de Windows Forms
+
+        /// <summary>
+        /// Método necesario para admitir el Diseñador. No se puede modificar
+        /// el contenido de este método con el editor de código.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.Datos = new System.Windows.Forms.TabControl();
+            this.tpgListado = new System.Windows.Forms.TabPage();
+            this.btnNuevo = new System.Windows.Forms.Button();
+            this.lvListado = new System.Windows.Forms.ListView();
+            this.Codigo = ((System.Windows.Forms.ColumnHeader)(new System.Windows.Forms.ColumnHeader()));
+            this.Nombre = ((System.Windows.Forms.ColumnHeader)(new System.Windows.Forms.ColumnHeader()));
+            this.Precio = ((System.Windows.Forms.ColumnHeader)(new System.Windows.Forms.ColumnHeader()));
+            this.Cantidad = ((System.Windows.Forms.ColumnHeader)(new System.Windows.Forms.ColumnHeader()));
+            this.tpgDatos = new System.Windows.Forms.TabPage();
+            this.button2 = new System.Windows.Forms.Button();
+            this.btnRegistrar = new System.Windows.Forms.Button();
+            this.groupBox1 = new System.Windows.Forms.GroupBox();
+            this.btnNombre = new System.Windows.Forms.Label();
+            this.lblPrecio = new System.Windows.Forms.Label();
+            this.lblCantidad = new System.Windows.Forms.Label();
+            this.lblCodigo = new System.Windows.Forms.Label();
+            this.textBox4 = new System.Windows.Forms.TextBox();
+            this.textBox3 = new System.Windows.Forms.TextBox();
+            this.textBox2 = new System.Windows.Forms.TextBox();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.Datos.SuspendLayout();
+            this.tpgListado.SuspendLayout();
+            this.tpgDatos.SuspendLayout();
+            this.groupBox1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // Datos
+            // 
+            this.Datos.Controls.Add(this.tpgListado);
+            this.Datos.Controls.Add(this.tpgDatos);
+            this.Datos.Location = new System.Drawing.Point(1, 2);
+            this.Datos.Name = "Datos";
+            this.Datos.SelectedIndex = 0;
+            this.Datos.Size = new System.Drawing.Size(684, 436);
+            this.Datos.TabIndex = 0;
+            // 
+            // tpgListado
+            // 
+            this.tpgListado.Controls.Add(this.btnNuevo);
+            this.tpgListado.Controls.Add(this.lvListado);
+            this.tpgListado.Location = new System.Drawing.Point(4, 22);
+            this.tpgListado.Name = "tpgListado";
+            this.tpgListado.Padding = new System.Windows.Forms.Padding(3);
+            this.tpgListado.Size = new System.Drawing.Size(676, 410);
+            this.tpgListado.TabIndex = 0;
+            this.tpgListado.Text = "Listado";
+            this.tpgListado.UseVisualStyleBackColor = true;
+            // 
+            // btnNuevo
+            // 
+            this.btnNuevo.Location = new System.Drawing.Point(7, 372);
+            this.btnNuevo.Name = "btnNuevo";
+            this.btnNuevo.Size = new System.Drawing.Size(75, 23);
+            this.btnNuevo.TabIndex = 2;
+            this.btnNuevo.Text = "Nuevo";
+            this.btnNuevo.UseVisualStyleBackColor = true;
+            // 
+            // lvListado
+            // 
+            this.lvListado.Columns.AddRange(new System.Windows.Forms.ColumnHeader[] {
+            this.Codigo,
+            this.Nombre,
+            this.Precio,
+            this.Cantidad});
+            this.lvListado.HideSelection = false;
+            this.lvListado.Location = new System.Drawing.Point(7, 6);
+            this.lvListado.Name = "lvListado";
+            this.lvListado.Size = new System.Drawing.Size(664, 353);
+            this.lvListado.TabIndex = 1;
+            this.lvListado.UseCompatibleStateImageBehavior = false;
+            this.lvListado.View = System.Windows.Forms.View.Details;
+            // 
+            // Codigo
+            // 
+            this.Codigo.Tag = "";
+            this.Codigo.Text = "Codigo";
+            this.Codigo.Width = 166;
+            // 
+            // Nombre
+            // 
+            this.Nombre.Text = "Nombre";
+            this.Nombre.Width = 140;
+            // 
+            // Precio
+            // 
+            this.Precio.Text = "Precio";
+            this.Precio.Width = 151;
+            // 
+            // Cantidad
+            // 
+            this.Cantidad.Text = "Cantidad";
+            this.Cantidad.Width = 198;
+            // 
+            // tpgDatos
+            // 
+            this.tpgDatos.Controls.Add(this.button2);
+            this.tpgDatos.Controls.Add(this.btnRegistrar);
+            this.tpgDatos.Controls.Add(this.groupBox1);
+            this.tpgDatos.Location = new System.Drawing.Point(4, 22);
+            this.tpgDatos.Name = "tpgDatos";
+            this.tpgDatos.Padding = new System.Windows.Forms.Padding(3);
+            this.tpgDatos.Size = new System.Drawing.Size(676, 410);
+            this.tpgDatos.TabIndex = 1;
+            this.tpgDatos.Text = "Datos";
+            this.tpgDatos.UseVisualStyleBackColor = true;
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(181, 278);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(75, 23);
+            this.button2.TabIndex = 2;
+            this.button2.Text = "button2";
+            this.button2.UseVisualStyleBackColor = true;
+            // 
+            // btnRegistrar
+            // 
+            this.btnRegistrar.Location = new System.Drawing.Point(14, 278);
+            this.btnRegistrar.Name = "btnRegistrar";
+            this.btnRegistrar.Size = new System.Drawing.Size(75, 23);
+            this.btnRegistrar.TabIndex = 1;
+            this.btnRegistrar.Text = "Registrar";
+            this.btnRegistrar.UseVisualStyleBackColor = true;
+            this.btnRegistrar.Click += new System.EventHandler(this.btnRegistrar_Click);
+            // 
+            // groupBox1
+            // 
+            this.groupBox1.Controls.Add(this.btnNombre);
+            this.groupBox1.Controls.Add(this.lblPrecio);
+            this.groupBox1.Controls.Add(this.lblCantidad);
+            this.groupBox1.Controls.Add(this.lblCodigo);
+            this.groupBox1.Controls.Add(this.textBox4);
+            this.groupBox1.Controls.Add(this.textBox3);
+            this.groupBox1.Controls.Add(this.textBox2);
+            this.groupBox1.Controls.Add(this.textBox1);
+            this.groupBox1.Location = new System.Drawing.Point(7, 6);
+            this.groupBox1.Name = "groupBox1";
+            this.groupBox1.Size = new System.Drawing.Size(325, 249);
+            this.groupBox1.TabIndex = 0;
+            this.groupBox1.TabStop = false;
+            // 
+            // btnNombre
+            // 
+            this.btnNombre.AutoSize = true;
+            this.btnNombre.Location = new System.Drawing.Point(47, 90);
+            this.btnNombre.Name = "btnNombre";
+            this.btnNombre.Size = new System.Drawing.Size(44, 13);
+            this.btnNombre.TabIndex = 7;
+            this.btnNombre.Text = "Nombre";
+            // 
+            // lblPrecio
+            // 
+            this.lblPrecio.AutoSize = true;
+            this.lblPrecio.Location = new System.Drawing.Point(47, 135);
+            this.lblPrecio.Name = "lblPrecio";
+            this.lblPrecio.Size = new System.Drawing.Size(37, 13);
+            this.lblPrecio.TabIndex = 6;
+            this.lblPrecio.Text = "Precio";
+            // 
+            // lblCantidad
+            // 
+            this.lblCantidad.AutoSize = true;
+            this.lblCantidad.Location = new System.Drawing.Point(47, 185);
+            this.lblCantidad.Name = "lblCantidad";
+            this.lblCantidad.Size = new System.Drawing.Size(49, 13);
+            this.lblCantidad.TabIndex = 5;
+            this.lblCantidad.Text = "Cantidad";
+            // 
+            // lblCodigo
+            // 
+            this.lblCodigo.AutoSize = true;
+            this.lblCodigo.Location = new System.Drawing.Point(47, 53);
+            this.lblCodigo.Name = "lblCodigo";
+            this.lblCodigo.Size = new System.Drawing.Size(40, 13);
+            this.lblCodigo.TabIndex = 4;
+            this.lblCodigo.Text = "Codigo";
+            // 
+            // textBox4
+            // 
+            this.textBox4.Location = new System.Drawing.Point(174, 87);
+            this.textBox4.Name = "textBox4";
+            this.textBox4.Size = new System.Drawing.Size(100, 20);
+            this.textBox4.TabIndex = 3;
+            // 
+            // textBox3
+            // 
+            this.textBox3.Location = new System.Drawing.Point(174, 132);
+            this.textBox3.Name = "textBox3";
+            this.textBox3.Size = new System.Drawing.Size(100, 20);
+            this.textBox3.TabIndex = 2;
+            // 
+            // textBox2
+            // 
+            this.textBox2.Location = new System.Drawing.Point(174, 182);
+            this.textBox2.Name = "textBox2";
+            this.textBox2.Size = new System.Drawing.Size(100, 20);
+            this.textBox2.TabIndex = 1;
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(174, 50);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(100, 20);
+            this.textBox1.TabIndex = 0;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.Datos);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Datos.ResumeLayout(false);
+            this.tpgListado.ResumeLayout(false);
+            this.tpgDatos.ResumeLayout(false);
+            this.groupBox1.ResumeLayout(false);
+            this.groupBox1.PerformLayout();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TabControl Datos;
+        private System.Windows.Forms.TabPage tpgListado;
+        private System.Windows.Forms.TabPage tpgDatos;
+        private System.Windows.Forms.ListView lvListado;
+        private System.Windows.Forms.ColumnHeader Codigo;
+        private System.Windows.Forms.ColumnHeader Nombre;
+        private System.Windows.Forms.ColumnHeader Precio;
+        private System.Windows.Forms.ColumnHeader Cantidad;
+        private System.Windows.Forms.GroupBox groupBox1;
+        private System.Windows.Forms.Label btnNombre;
+        private System.Windows.Forms.Label lblPrecio;
+        private System.Windows.Forms.Label lblCantidad;
+        private System.Windows.Forms.Label lblCodigo;
+        private System.Windows.Forms.TextBox textBox4;
+        private System.Windows.Forms.TextBox textBox3;
+        private System.Windows.Forms.TextBox textBox2;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.Button btnNuevo;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Button btnRegistrar;
+    }
+}
+
---
+++ Sem_06_Ordenamiento/PryMetBubble/clsDatos.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PryMetBubble
+{
+    internal class clsDatos
+    {
+        public int Codigo { get; set; }
+        public int Nombre { get; set; }
+        public int Precio { get; set; }
+        public int Cantidad { get; set; }
+
+        public clsDatos(int codigo, int nombre, int precio, int cantidad)
+        {
+            Codigo = codigo;
+            Nombre = nombre;
+            Precio = precio;
+            Cantidad = cantidad;
+        }
+
+    }
+}
